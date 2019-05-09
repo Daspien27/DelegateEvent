336a134c2631049d168bae5152e41ff336a44113
Amund Desmarais <amunddesmarais64@gmail.com>
2019-05-09 18:37:41 -0400

    Initial Version

--- DelegateEvent/DelegateEvent.cpp
+++ DelegateEvent/DelegateEvent.cpp
@@ -3,19 +3,106 @@
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+//public class MyClass
+//{
+//	public delegate bool MyDel();
+//	public event MyDel MyEvent;
+//
+//	public MyClass()
+//	{
+//		MyEvent += foo1;
+//		MyEvent += foo2;
+//	}
+//
+//	public bool InvokeEvent()
+//	{
+//		return MyEvent ? .Invoke();
+//	}
+//
+//	public bool foo1() { return true; }
+//	public bool foo2() { return false; }
+//}
+//
+//int Main()
+//{
+//	MyClass classObject = new MyClass();
+//	printf(classObject.InvokeEvent());
+//}
+
+template <typename T> struct FuncExtractor;
+
+template <typename Return, typename... Params>
+struct FuncExtractor< Return (*)(Params...)>
+{
+	using ReturnType = typename Return;
+//	using ParamsType = typename Params...; //Can we do this?
+};
+
+template <typename DelegateSignature>
+struct Event
+{
+	using ReturnType = typename FuncExtractor<DelegateSignature>::ReturnType;
+	std::vector<DelegateSignature> MyList;
+
+	Event& operator += (const DelegateSignature& NewRegister)
+	{
+		MyList.push_back(NewRegister);
+
+		return *this;
+	}
+
+	template <typename... Args>
+	ReturnType Invoke(Args&&... args)
+	{
+		if (MyList.empty())
+		{
+			throw std::bad_function_call(); //Could use a template parameter to define default behaviours here, i.e. returning a default initialize value instead.
+		}
+
+		std::for_each(MyList.begin(), std::next(MyList.begin(), MyList.size() - 1), [&](const auto& Func)
+		{
+			Func(args...);
+		});
+
+		return MyList.back()(args...);
+	}
+};
+
+
+struct MyClass
+{
+	using Delegate = bool(*)();
+
+	Event<Delegate> MyEvent;
+
+	MyClass()
+	{
+		MyEvent += alwaysTrue;
+		MyEvent += alwaysFalse;
+	}
+
+	bool InvokeEvent()
+	{
+		return MyEvent.Invoke();
+	}
+
+	static bool alwaysTrue()
+	{
+		return true;
+	}
+
+	static bool alwaysFalse()
+	{
+		return false;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	MyClass classObject;
+	std::cout << std::boolalpha << classObject.InvokeEvent();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
